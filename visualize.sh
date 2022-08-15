2db35892b65bcdbb0ed962a1d069026031658e43
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2022-08-15 16:18:34 +0300

    Clean-up

--- visualize.sh
+++ visualize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ./generator --big-superposition > map_from_generator
-./beautymapper_rust map_from_generator $1
+./beautymapper_mac map_from_generator $1
 ./lem-in -paths -file map_from_generator-beautified-$1 > beautified
-./visualizer_rust beautified 
+./visualizer_mac beautified 
